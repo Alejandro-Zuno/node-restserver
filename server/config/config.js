af8bcaac50c3d6e959a41235620ebdd6845065ac
Alejandro <alejandrozuno@hotmail.com>
2019-04-18 10:49:09 -0500

    Rest Server - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,21 @@
 // ======================
 
 process.env.PORT = process.env.PORT || 3000;
+
+// ======================
+// Entorno 
+// ======================
+process.env.NODE_ENV == process.env.NODE_ENV || 'dev';
+
+// ======================
+// Base de datos 
+// ======================
+
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://admin:V8CrqzGdamn9U1sA@cluster0-biguq.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
