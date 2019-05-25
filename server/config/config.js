8109f6c4988b25847be0c6fb89edbb743f11b26d
Alejandro <alejandrozuno@hotmail.com>
2019-05-25 10:07:26 -0500

    Fixing config issue

--- server/config/config.js
+++ server/config/config.js
@@ -7,7 +7,7 @@ process.env.PORT = process.env.PORT || 3000;
 // ======================
 // Entorno 
 // ======================
-process.env.NODE_ENV == process.env.NODE_ENV || 'dev';
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // ======================
 // Vencimiento de Token 
@@ -32,7 +32,4 @@ if (process.env.NODE_ENV === 'dev') {
     urlDB = process.env.MONGO_URI;
 }
 
-
-urlDB = 'mongodb://localhost:27017/cafe';
 process.env.URLDB = urlDB;
-console.log(urlDB);
